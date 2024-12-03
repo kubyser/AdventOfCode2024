7f5cf73ec907210b4b6bcbdd6ace61c2103d8bcf
Sergey Kubyshin <kubiks@gmail.com>
2024-12-03 11:14:49 +0000

    Day 3

--- Day3.py
+++ Day3.py
@@ -1,3 +1,11 @@
-f = open("resources/day3_input.txt", "r")
-data = [list(map(int, l.split(" "))) for l in f.read().splitlines()]
-f.close()
+import re
+from functools import reduce
+from operator import concat, mul
+
+data = "do()" + reduce(concat, open("resources/day3_input.txt", "r").readlines())
+calculate = lambda program: sum(reduce(mul, map(int, l.split(","))) for l in re.findall(r'mul\((\d+,\d+)\)', program))
+print(calculate(data))
+data = reduce(concat, [s[s.find("do()")+len("do()"):] for s in data.split("don\'t()") if "do()" in s])
+print(calculate(data))
+
+
