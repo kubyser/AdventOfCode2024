7cc388e9df3a4f608b1c29877055d66d2a44fd83
Sergey Kubyshin <kubiks@gmail.com>
2024-12-21 10:54:00 +0000

    Day 21 clean up

--- Day21.py
+++ Day21.py
@@ -14,44 +14,41 @@ def horSeq(keypad, startPos, endPos):
 def vertSeq(keypad, startPos, endPos):
     return ('v' if endPos[1]>startPos[1] else '^')* abs(endPos[1]-startPos[1])
 
-@functools.cache
+def getMovementSequence(oldPos, newPos, keypad):
+    c = keypad[oldPos[1]][oldPos[0]]
+    if oldPos == newPos:
+        return 'A'
+    elif newPos[1] == oldPos[1]:
+        return horSeq(keypad, oldPos, newPos) +'A'
+    elif c in '<>' or c in '741' and oldPos[1]==3 or newPos[0]>oldPos[0] and (c in '235689' or c in '0A' and oldPos[0]!=0):
+        return vertSeq(keypad, oldPos, newPos) + horSeq(keypad, oldPos, newPos) + 'A'
+    else:
+        return horSeq(keypad, oldPos, newPos) + vertSeq(keypad, oldPos, newPos) + 'A'
+
+
 def getSequence(targetSequence, keypad, startChar):
     pos = getCoords(keypad, startChar)
     sequence = ''
     for c in targetSequence:
         newPos = getCoords(keypad, c)
-        if pos == newPos:
-            sequence += 'A'
-        elif newPos[1] == pos[1]:
-            sequence += horSeq(keypad, pos, newPos) +'A'
-        elif c in '<>' or c in '741' and pos[1]==3 or newPos[0]>pos[0] and (c in '235689' or c in '0A' and pos[0]!=0):
-            sequence += vertSeq(keypad, pos, newPos) + horSeq(keypad, pos, newPos) + 'A'
-        else:
-            sequence += horSeq(keypad, pos, newPos) + vertSeq(keypad, pos, newPos) + 'A'
+        sequence += getMovementSequence(pos, newPos, keypad)
         pos = newPos
     return sequence
 
 
 @functools.cache
-def generateLength(c, prevC, depth):
+def generateLength(c, prevC, depth, keypad):
     pos = getCoords(DIRECTIONAL_KEYPAD, prevC)
     sequence = ''
     newPos = getCoords(DIRECTIONAL_KEYPAD, c)
-    if pos == newPos:
-        sequence += 'A'
-    elif newPos[1] == pos[1]:
-        sequence += horSeq(DIRECTIONAL_KEYPAD, pos, newPos) +'A'
-    elif c in '<>' or c in '741' and pos[1]==3 or newPos[0]>pos[0] and (c in '235689' or c in '0A' and pos[0]!=0):
-        sequence += vertSeq(DIRECTIONAL_KEYPAD, pos, newPos) + horSeq(DIRECTIONAL_KEYPAD, pos, newPos) + 'A'
-    else:
-        sequence += horSeq(DIRECTIONAL_KEYPAD, pos, newPos) + vertSeq(DIRECTIONAL_KEYPAD, pos, newPos) + 'A'
+    sequence += getMovementSequence(pos, newPos, DIRECTIONAL_KEYPAD)
     if depth == 1:
         #print(f"Char: {c}, depth={depth}, Full: {sequence}")
         return len(sequence)
     fullSequence = 0
     pc = 'A'
     for newChar in sequence:
-        fullSequence += generateLength(newChar, pc, depth-1)
+        fullSequence += generateLength(newChar, pc, depth-1, DIRECTIONAL_KEYPAD)
         pc = newChar
     #print(f"Char: {c}, depth={depth}, Full: {fullSequence}")
     return fullSequence
@@ -62,17 +59,27 @@ print(codes)
 keypads = [NUMERIC_KEYPAD] + [DIRECTIONAL_KEYPAD]*25
 finalSequences = []
 
+def troubleshoot():
+    for seq in ['<^A', '^<A', '>vA', 'v>A']:
+        res = seq + ' (' + str(len(seq)) + ')'
+        for i in range(2):
+            seq = getSequence(seq, DIRECTIONAL_KEYPAD, 'A')
+            res += '  ->  ' + seq + ' (' + str(len(seq)) + ')'
+        print(res)
+    exit(0)
+
+#troubleshoot()
+
+NUM_NUMPADS = 25
 for code in codes:
     #    positions = ['A']*len(keypads)
     targetSeq = getSequence(code, NUMERIC_KEYPAD, 'A')
     seq = 0
-    print(f"target: {targetSeq}")
     pc = 'A'
     for c in targetSeq:
-        seq += generateLength(c, pc, 25)
+        seq += generateLength(c, pc, NUM_NUMPADS, DIRECTIONAL_KEYPAD)
         pc = c
-        #print(seq)
     print(f"Code: {code}, {seq}")
     finalSequences.append(seq)
 score = [finalSequences[i]*int(codes[i].removesuffix('A')) for i in range(len(codes))]
-print(f"Score: {sum(score)}")
+print(f"Final score: {sum(score)}")
